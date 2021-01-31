339ea8f87d2e51f2b26c5e024b10ee5abcd31b5c
Ivo Cardozo Vargas <Ivo.Cardozo@jalasoft.com>
2021-01-31 10:21:44 -0400

    starting project

---
+++ ReactTest/ClientApp/src/components/displayFields/DisplayFields.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from 'react';
+import axios from 'axios';
+
+class DisplayFields extends Component {
+    componentDidMount() {
+        axios.get('/WeatherForecast')
+            .then(response => {
+                console.log(response)
+            })
+            .catch(e => {
+                console.log("error" + e)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    TestReactApp testing on local environment
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default DisplayFields;
+
